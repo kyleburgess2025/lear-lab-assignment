16400d87cb59278cf9aaec7cf65ac6879836c33f
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2024-02-12 16:29:11 -0600

    Pushing to redeploy

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,7 +11,7 @@ export default function BlogHome() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1 className="text-4xl font-bold">Blog</h1>
-        <h2 className="text-2xl font-bold">Welcome to my blog! Here, we have a large number of cool things.</h2>
+        <h2 className="text-2xl font-bold">Welcome to the Kyle Blog! Here, we have a large number of cool things.</h2>
         {jsonData.map((blog: Blog) => {
             return (
                 <a href={`blog/${blog.id}`} key={blog.id} className="flex items-center justify-between w-96 p-3 border border-gray-300 rounded-lg">
